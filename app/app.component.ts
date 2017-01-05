15ba907add614b3ee757b2f2eaf96334a0645461
adavis32 <adeand25@gmail.com>
2017-01-05 14:38:17 -0500

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: '<h1>Skeleton Project</h1>'
 })
-export class AppComponent {}
+export class AppComponent { }
